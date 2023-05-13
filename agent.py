46e557ba5f8b7c8faad409626aec64696d9fb515
Cocostee <clementld76@gmail.com>
2023-05-13 02:40:21 +0200

    Full correction snake IA

--- agent.py
+++ agent.py
@@ -2,27 +2,25 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameIA, Direction, Point
-from model import Linear_Qnet, QTrainer
+from game import SnakeAI,Direction,Point
+from model import Linear_Qnet,QTrainer
 from helper import plot
-
-MAX_MEMORY = 100_000
-BATCH_SIZE = 1000
-LR = 0.001
-
+MaxMemory=100_00
+BATCH_SIZE=1000
+LearningRate=0.001
 
 class Agent:
 
     def __init__(self):
-        self.n_games = 0
-        self.epsilon = 0  # random exploration
-        self.gamma = 0.9  # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
-        self.model = Linear_Qnet(11, 256, 3)  # model
-        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)  # trainer
-        # model, trainer
+        self.n_games=0
+        self.epsilon=0 #randomness
+        self.gamma=0.9 #discount rate
+        self.memoire=deque(maxlen=MaxMemory) #Vire les premiers elements quand depasse memoire
+        self.model = Linear_Qnet(11,256,3)
+        self.trainer = QTrainer(self.model,lr=LearningRate,gamma=self.gamma)
+        #TODO:model,trainer
 
-    def get_state(self, game):
+    def getstate(self,game):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
@@ -35,111 +33,106 @@ class Agent:
         dir_d = game.direction == Direction.DOWN
 
         state = [
-            # danger straight
-            (dir_r and game.is_collision(point_r)) or
-            (dir_l and game.is_collision(point_l)) or
-            (dir_u and game.is_collision(point_u)) or
-            (dir_d and game.is_collision(point_d)),
+            # Danger straight
+            (dir_r and game.isCollision(point_r)) or
+            (dir_l and game.isCollision(point_l)) or
+            (dir_u and game.isCollision(point_u)) or
+            (dir_d and game.isCollision(point_d)),
 
-            # danger right
-            (dir_u and game.is_collision(point_r)) or
-            (dir_d and game.is_collision(point_l)) or
-            (dir_l and game.is_collision(point_u)) or
-            (dir_r and game.is_collision(point_d)),
+            # Danger right
+            (dir_u and game.isCollision(point_r)) or
+            (dir_d and game.isCollision(point_l)) or
+            (dir_l and game.isCollision(point_u)) or
+            (dir_r and game.isCollision(point_d)),
 
-            # danger left
-            (dir_d and game.is_collision(point_r)) or
-            (dir_u and game.is_collision(point_l)) or
-            (dir_r and game.is_collision(point_u)) or
-            (dir_l and game.is_collision(point_d)),
+            # Danger left
+            (dir_d and game.isCollision(point_r)) or
+            (dir_u and game.isCollision(point_l)) or
+            (dir_r and game.isCollision(point_u)) or
+            (dir_l and game.isCollision(point_d)),
 
-            # move direction
-            dir_l, dir_r, dir_u, dir_d,
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
 
             # Food location
-            game.food.x < game.head.x,  # food left
-            game.food.y > game.head.y,  # food right
-            game.food.x < game.head.x,  # food up
-            game.food.y > game.head.y,  # food down
+            game.food.x < game.tete.x,  # food left
+            game.food.x > game.tete.x,  # food right
+            game.food.y < game.tete.y,  # food up
+            game.food.y > game.tete.y  # food down
         ]
-            
-        
 
         return np.array(state, dtype=int)
 
-    def remenber(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+    def remember(self,state,action,reward,nextState,done):
+        self.memoire.append((state,action,reward,nextState,done))
 
-    def train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE)
+    def trainLongMemory(self):
+        if len(self.memoire) > BATCH_SIZE:
+            miniSample=random.sample(self.memoire,BATCH_SIZE)#liste de tuples
         else:
-            mini_sample = self.memory
-        states, actions, rewards, next_states, dones = zip(*mini_sample)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)
+            miniSample=self.memoire
+        states,actions,rewards,nextStates,dones=zip(*miniSample)
+        self.trainer.trainStep(states, actions, rewards, nextStates, dones)
 
-    def train_short_memory(self, state, action, reward, next_state, done):
-        self.trainer.train_step(state, action, reward, next_state, done)
+    def trainShortMemory(self, state, action, reward, next_state, done):
+        self.trainer.trainStep(state, action, reward, next_state, done)
 
-    def get_action(self, state):
-        # random moves : tradeoff exploration / exploitation
-        self.epsilon = 80-self.n_games
-        final_move = [0, 0, 0]
-        if random.randint(0, 200) < self.epsilon:
-            move = random.randint(0, 2)
-            final_move[move] = 1
+    def getAction(self,state):
+        self.epsilon=80-self.n_games
+        finalMove=[0,0,0]
+        if random.randint(0,200)< self.epsilon:
+            move=random.randint(0,2)
+            finalMove[move]=1
         else:
-            state0 = torch.tensor(state, dtype=torch.float)
+            state0=torch.tensor(state,dtype=torch.float)
             prediction = self.model(state0)
-            move = torch.argmax(prediction).item()
-            final_move[move] = 1
+            move=torch.argmax(prediction).item()
+            finalMove[move]=1
+        return finalMove
 
-        return final_move
 
 
 def train():
-    plot_scores = []
-    plot_mean_scores = []
-    total_score = 0
-    record = 0
-    agent = Agent()
-    game = SnakeGameIA()
+    plotScore=[]
+    plotMeanScore1=[]
+    totalScore=0
+    best=0
+    agent=Agent()
+    game=SnakeAI()
     while True:
         #Avoir le dernier etat
-        state_old = agent.get_state(game)
-
-        # get move
-        final_move = agent.get_action(state_old)
-
-        # perform action
-        reward, done, score = game.play_step(final_move)
-        state_new = agent.get_state(game)
-
-        # train short memory
-        agent.train_short_memory(
-            state_old, final_move, reward, state_new, done)
-
-        # remember
-        agent.remenber(state_old, final_move, reward, state_new, done)
-
+        stateOld = agent.getstate(game)
+        #getMove
+        final_move=agent.getAction(stateOld)
+        #ameliore le move
+        reward,done,score=game.etapeDuJeu(final_move)
+        stateNew=agent.getstate(game)
+        #entraine la short memory
+        agent.trainShortMemory(stateOld,final_move,reward,stateNew,done)
+        #retient
+        agent.remember(stateOld,final_move,reward,stateNew,done)
         if done:
-            # train long memory, plot results
+            #entraine long memory
             game.reset()
             agent.n_games += 1
-            agent.train_long_memory()
+            agent.trainLongMemory()
 
-            if score > record:
-                record = score
+            if score > best:
+                best=score
                 agent.model.save()
 
+            print('Partie jouée :',agent.n_games,'Score :',score,'Record :',best)
+            plotScore.append(score)
+            totalScore += score
+            plotMeanScore=totalScore/agent.n_games
+            plotMeanScore1.append(plotMeanScore)
+            plot(plotScore,plotMeanScore1)
 
-            print('Game', agent.n_games, 'Score', score, 'Record', record)
 
-            plot_scores.append(score)
-            total_score += score
-            mean_score = total_score/agent.n_games
-            plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+    pass
 
-if __name__ == '__main__':
+if __name__ =='__main__':
     train()
